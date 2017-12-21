570707338d730100ab02d3f51ebade2bb359dbce
Jair Pereira <jair.pereira@aluno.ufabc.edu.br>
2017-12-21 01:14:58 -0200

    Revert "Update DSatur"
    This reverts commit 9ea2261e88958a68834922f80b70ae39a0c60cf1.

--- R/solver_dsatur.R
+++ R/solver_dsatur.R
@@ -3,14 +3,15 @@
 #' Solves the 3-GCP problem using DSatur heuristic
 #'
 #' DSatur heuristic function, adapted from the description by Daniel Brelaz,
-#' "New methods to Color the Vertices of a Graph" Communications of the ACM. 22(4): 251?..256 (1979).
+#' "New methods to Color the Vertices of a Graph" Communications of the ACM. 22(4): 251–256 (1979).
 #'
 #' The algorithm follows 5 steps:
-#' 1. Arrange the vertices by decreasing order of degrees (or any given permutation).
+#' 1. Arrange the vertices by decreasing order of degrees (or any other permutation).
 #' 2. Color a vertex of maximal degree with color 1.
 #' 3. Choose a vertex with a maximal saturation degree.
 #' 4. Color the chosen vertex with the least possible (lowest numbered) color.
 #' 5. If all the vertices are colored, stop. Otherwise, return to 3.
+#'
 #'
 #' @param G the graph to be solved, represented by a list where G$V is the number of nodes, G$E is a |E|x2 edge matrix,
 #' G$adj is the adjacency list.
@@ -19,7 +20,8 @@
 #' following names:
 #' \itemize{
 #' \item \emph{weight}: List of weights, which will be used to build a permutation of vertices.
-#' \item \emph{return_satur}: Boolean, if the final saturation degrees should be returned (used by some methods like heuristical swap)
+#' \item \emph{partial_solution}: List of colors. Partial solution for G.
+#' \item \emph{leave_uncolored}: Boolean. If it is allowed to have uncolored vertices (then, no violation) in the solution
 #' }
 #'
 #' @return a list containing the total number of violations of the best coloring, the best coloring
@@ -30,30 +32,75 @@
 solver_dsatur <- function(G, nfe, args){#wrapper
   assertthat::assert_that(
     all(assertthat::has_name(args,
-                             c("weight", "return_satur")
+                             c("weight", "partial_solution", "leave_uncolored")
     )))
 
   # Parameters:
   weight <- args[["weight"]]
-  return_satur <- args[["return_satur"]]
+  partial_solution <- args[["partial_solution"]]
+  leave_uncolored <- args[["leave_uncolored"]]
 
-  result <- dsatur(G, nfe, weight) #doesn't color all vertices, doesn't generate violation
-
-  if(return_satur == TRUE){
-    r <- list(violation = result[["violation"]], best=result[["best"]], evals=result[["evals"]], satur=result[["satur"]])
-  } else{
-    r <- list(violation = result[["violation"]], best=result[["best"]], evals=result[["evals"]])
+  if(is.null(partial_solution)){
+    partial_solution <- sample(0, G$V, replace=T)
   }
 
-  return(r)
+  if(leave_uncolored == T){
+    result <- dsatur2(G, nfe, weight, partial_solution) #doesn't color all vertices, doesn't generate violation
+  }else{
+    result <- dsatur(G, nfe, weight, partial_solution) #colors all vertices, can generate violation
+  }
+
+  return(list(violation = result[["violation"]], best=result[["best"]], evals=result[["evals"]]))
 
 }
 
-#' DSatur (doesn't color all vertices, doesn't generate violation)
-dsatur <- function(G, nfe, weight){
+#' DSatur (colors all vertices, can generate violation)
+dsatur <- function(G, nfe, weight, partial_solution){
   #Init: solution, adjacent colors of each vertex, saturation degrees
-  solution <- sample(0, G$V, replace=T)
-  adjacent_color <- matrix(0, 3, G$V)
+  solution <- partial_solution
+  adjacent_color <- adjacent_color_set(solution, G)
+
+  #satur
+  #satur <- rep(0, G$V)
+  uncolored_v <- which(solution==0)
+  satur <- sapply(1:G$V, FUN = function(x) { if(is.element(x, uncolored_v)) {sum(adjacent_color[,x])} else -1 })
+
+  #if tie in saturation, use this permutation
+  perm <- order(weight, decreasing = T)
+
+  nit <- 0
+  while((nit < nfe) & (length(solution[solution == 0])>0)){
+    #Next vertex
+    v <- perm[which(satur[perm] == max(satur))[1]]
+
+    #Lowest Numbered color
+    color <- which.min(adjacent_color[,v])
+
+    #Color vertex
+    solution[v] <- color
+
+    #update counter of adjacent colors
+    adjacent_color[color, G$adj[[as.character(v)]]] <- 1
+    adjacent_color[color, v] <- 1
+
+    #keep track of uncolored vertices
+    uncolored_v <- which(solution==0)
+
+    #update saturation degree (and reduce priority of already colored vertices)
+    satur <- sapply(1:G$V, FUN = function(x) { if(is.element(x, uncolored_v)) {sum(adjacent_color[,x])} else -1 })
+
+    nit <- nit + 1
+  }
+
+  violation <- evaluate(solution, G)
+  return(list(violation = violation, best = solution, evals = nit))
+}
+
+#' DSatur (doesn't color all vertices, doesn't generate violation)
+dsatur2 <- function(G, nfe, weight, partial_solution){
+  #Init: solution, adjacent colors of each vertex, saturation degrees
+  solution <- partial_solution
+  adjacent_color <- adjacent_color_set(solution, G)
 
   colored <- which(solution!=0) #set of colored vertices and vertices that could not be colored due to violation.
   satur <- sapply(1:G$V, FUN = function(x) { sum(adjacent_color[,x]) })
@@ -67,15 +114,12 @@ dsatur <- function(G, nfe, weight){
     #Next vertex
     v <- perm[which(satur[perm] == max(satur))[1]]
 
-    #possible colors for the vertex v
-    colors <- which(adjacent_color[,v] == 0)
+    #Lowest Numbered color
+    color <- which.min(adjacent_color[,v])
 
     #if it is possible to color v without violation, color it
     #otherwise, leave it with no color
-    if(length(colors) > 0){
-      #color <- sample(colors, 1) #choose one color randomly
-      color <- colors[1] #lowest numbered color
-
+    if(adjacent_color[color,v] == 0){
       #Color vertex
       solution[v] <- color
 
@@ -93,16 +137,14 @@ dsatur <- function(G, nfe, weight){
     nit <- nit + 1
   }
 
-  violation <- sum(solution==0)
-  evals2 <- ((G$V^2) + nrow(G$E)) / nrow(G$E)
-  satur <- sapply(1:G$V, FUN = function(x) { sum(adjacent_color[,x]) })
+  violation <- evaluate(solution, G)
+  violation <- violation + sum(solution==0)
 
-  return(list(violation = violation, best = solution, evals = evals2, satur = satur))
+  return(list(violation = violation, best = solution, evals = nit))
 }
 
 #' Auxiliar function for DSatur
-#' Given a set of colors of G, compute adjancent colors to each vertex
-#' This is an auxiliary struture to compute the saturation degrees of G
+#' Given a set of partial coloring of G, compute adjancent colors to each vertex
 #' @param s, set of colors, size |V|
 #' @return a matrix (col: vertices, row: colors) containing the colors adjacent to each vertex
 #' (a V vector of 1:3) and the total number of evaluations spent
